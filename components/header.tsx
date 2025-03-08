904e7a2575fddd875c77600d6dd3b1f6f3c3fb57
unknown <juliansantanatoglia@gmail.com>
2025-03-08 12:12:21 -0300

    social networks agregadas

---
+++ components/header.tsx
@@ -0,0 +1,33 @@
+import Link from "next/link";
+import MotionTransition from "./transition-component";
+import { socialNetworks } from "../data";
+
+const Header = () => {
+    return (
+        <MotionTransition position="bottom" className="absolute <-40 inline-block w-full top-5  md:top-10">
+            <header>
+                <div className="container justify-between max-w-6xl mx-auto md:flex">
+                    <Link href="/">
+                    <h1 className="my-3 text-4xl font-bold text-center md:text-left">
+                        Julián
+                        <span className="text-sky-600 ">Santanatoglia</span>
+                    </h1>
+                    </Link>
+                    <div className="flex items-center justify-center gap-7">
+                        {socialNetworks.map(({logo, src, id}) => (
+                            <Link key={id}
+                            href={src}
+                            target="_blank">
+                                {logo}
+                            </Link>
+                        ))}
+{/* MIN 47:02 */}
+                    </div>
+                </div>
+            </header>
+
+        </MotionTransition>
+    );
+}
+
+export default Header;
